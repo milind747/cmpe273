9340feb08c0310661a04db787af04f49a4c8bdd5
Milind Patel <milindpatel3012@gmail.com>
2017-10-17 23:29:06 -0700

    Add files via upload

--- assignment1/foo.py
+++ assignment1/foo.py
@@ -1,29 +1,35 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, make_response, request
+import rocksdb, uuid
+import subprocess
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id':1,
-        'title':u'Buy groceries',
-        'description':u'milk,cheese,pizza,fruit',
-        'done':False
-    },
-    {
-        'id':2,
-        'title':u'learn Phython',
-        'description':u'need to find a good python tutorial on the web',
-        'done':False
-    }
-]
 
-@app.route("/todo/api/v1.0/tasks<int:task_id>", methods=['GET'])
+@app.route('/api/v1/scripts',methods=['POST'])
+def save_file():
+    inputdb = rocksdb.DB("cmpe273assn1.db",rocksdb.Options(create_if_missing = True))
+    k = uuid.uuid4().hex
+    a = request.files['data']
+    mainfile = '/tmp/' + k + '.py'
+    a.save(mainfile)
+    inputdb.put(k.encode('utf-8'),mainfile.encode('utf-8'))
+    return (k)
 
-def get_tasks(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'tasks': task[0]})
+
+@app.route('/api/v1/scripts/<script_id>',methods=['GET'])
+def call_file(script_id):
+    inputdb = rocksdb.DB("cmpe273assn1.db",rocksdb.Options(create_if_missing = True))
+    xy = inputdb.get(script_id.encode('utf-8'))
+    tostr = str(xy)
+    fnlstr=tostr[2:]
+    fnlstr1="python3 " + fnlstr[0:len(fnlstr)-1]
+    
+    p = subprocess.Popen(fnlstr1, stdout=subprocess.PIPE, shell=True)
+    #print p.communicate()
+    return p.stdout.readline()
+
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
